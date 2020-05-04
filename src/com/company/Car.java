91574b502a375379092c39edf10a9ccb76c0d0c8
jderesz <janusz@pdot.eu>
2020-05-05 01:39:11 +0200

    zad 5

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -4,6 +4,7 @@ final public class Car {
     final String model;
     final String producer;
     private Double weight = 100.0;
+    public Double value = 100.0;
 
     public Car(String model, String producer) {
         this.model = model;
--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -6,7 +6,15 @@ public final class Human {
     private Double salary = 0.0;
 
     public void setCar(Car car) {
-        this.car = car;
+        if(this.salary > car.value) {
+            System.out.println("udało się kupić za gotówkę");
+            this.car = car;
+        } else if(this.salary > car.value/12) {
+            System.out.println("udało się kupić na kredyt (no trudno) i przypisujemy samochód");
+            this.car = car;
+        } else {
+            System.out.println("zapisz się na studia i znajdź nową robotę albo idź po podwyżkę");
+        }
     }
 
     public void setSalary(Double salary) {
@@ -17,5 +25,9 @@ public final class Human {
         System.out.println("Pamiętaj aby odebrać aneks do umowy od pani Hani z kadr.");
         System.out.println("ZUS i US już wiedzą o zmianie wypłaty i nie ma sensu ukrywać dochodu!");
         this.salary = salary;
+    }
+
+    public Car getCar() {
+        return car;
     }
 }
